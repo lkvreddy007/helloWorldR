c4b21ee172de0ca8939aeec2756a24b9c57e9619
lkvreddy007 <lkvreddy007@gmail.com>
2020-11-28 23:10:02 +0530

    [Krishna]added-helloMessageUsingOneWayDataBindings

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -2,13 +2,21 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component{
+
+  constructor(){
+    super();
+    this.state = {
+      title: 'Hello From Krishna'
+    }
+  }
   render(){
     return(
       <div>
-        <h1>Hello From Krishna</h1>
+        <h1>{this.state.title}</h1>
       </div>
     )
   }
+
 }
 
 export default App;
