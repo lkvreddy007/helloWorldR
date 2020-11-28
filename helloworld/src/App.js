03d5c57fc20a28c6d3fcde90d7eb23e620e109a8
lkvreddy007 <lkvreddy007@gmail.com>
2020-11-29 00:00:21 +0530

    [Krishna]added-toDisplayHelloFromKrishnaWithAUserInputNameWithRegex

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -7,31 +7,40 @@ class App extends React.Component{
   constructor(){
     super();
     this.state = {
-      userName: ''
+      userName: '',
+      nameError: ''
     }
   }
+
   onClick = ($event) => {
-    console.log("save button is clicked",$event);
-    window.open(this.url,"_blank")
+    console.log("save button is clicked", $event);
+    window.open(this.url,"_blank");
   }
   onNameChange = (event) => {
     console.log("values is ",event.target.value);
-    this.setState({userName: event.target.value})
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    this.setState({userName: event.target.value});
+    if(nameRegex.test(event.target.value)){
+      this.setState({nameError: ''});
+    }
+    else{
+      this.setState({nameError: 'Name is Incorrect'});
+    }
   }
   render(){
     return(
       <div>
         <div>
           <h1>Hello {this.state.userName} from Krishna</h1>
-          <img src={logo} onClick={this.onClick} className="App-logo" alt="logo" />
+          <img src={logo} onClick={this.onClick} alt="logo" />
         </div>
         <div>
-          <input onChange={this.onNameChange}/>
+          <input onChange={this.onNameChange} />
+          <span className="error-output">{this.state.nameError}</span>
         </div>
       </div>
     )
   }
-
 }
 
 export default App;
